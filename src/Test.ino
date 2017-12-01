f8e489582f098f58601c6338b4c94053f6f4d7d5
LENOVOW540\jez <jez@kingjez.com>
2017-12-01 22:13:01 +0000

    Starter

---
+++ src/Test.ino
@@ -0,0 +1,31 @@
+/*
+ * Project Test
+ * Description:
+ * Author:
+ * Date:
+ */
+int led1 = D0;
+int led2 = D7;
+
+// setup() runs once, when the device is first turned on.
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+
+  // We'll leave it on for 1 second...
+  delay(1000);
+
+  // Then we'll turn it off...
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+
+  // Wait 1 second...
+  delay(1000);
+}
